c20e74b4462cc7408c131601b76f3334d36ba432
Spencer Cornelia <cornelia.spencer@yahoo.com>
2017-09-26 06:36:14 -0700

    added investor resource

---
+++ Controllers/resources/InvestorResource.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Veeya.Models;
+
+namespace Veeya.Controllers.Resources
+{
+    public class InvestorResource
+    {
+        public int id { get; set; }
+        
+        public string First_Name { get; set; }
+
+        public string Last_Name { get; set; }
+
+        public string Email_Address { get; set; }
+       
+        public string Phone_Number { get; set; }
+
+        public ICollection<Wholesaler> Wholesalers { get; set; }
+
+        public InvestorResource()
+        {
+            Wholesalers = new Collection<Wholesaler>();
+        }
+    }
+}

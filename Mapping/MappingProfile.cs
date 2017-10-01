eacc870ada318781ff72fd160cbbdd0840453fc9
Spencer Cornelia <cornelia.spencer@yahoo.com>
2017-10-01 08:49:04 -0700

    added propertyResource mapping back to Property

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -12,6 +12,14 @@ namespace Veeya.Mapping
             CreateMap<Property, PropertyResource>();
             CreateMap<Investor, InvestorResource>();
             CreateMap<InvestorToWholesaler, InvestorToWholesalerResource>();
+
+            // used for posting to /api/properties. need to convert propertyResource
+            // back to Property. add additional Property properties here when the time
+            // comes (city, state, zip, etc.)
+            CreateMap<PropertyResource, Property>()
+                .ForMember(p => p.PropertyId, opt => opt.MapFrom(pr => pr.PropertyId))
+                .ForMember(p => p.AddressName, opt => opt.MapFrom(pr => pr.AddressName))
+                .ForMember(p => p.WholesalerId, opt => opt.MapFrom(pr => pr.WholesalerId));
         }
     }
 }

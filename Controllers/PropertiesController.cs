050ce83e88db1559db593a127180b23887933144
Spencer Cornelia <cornelia.spencer@yahoo.com>
2017-10-01 13:21:17 -0700

    added update, get, and delete http requests

--- Controllers/PropertiesController.cs
+++ Controllers/PropertiesController.cs
@@ -24,19 +24,72 @@ namespace Veeya.Controllers
         }
 
         [HttpPost]
-        public IActionResult CreateProperty([FromBody] PropertyResource propertyResource)
+        public async Task<IActionResult> CreateProperty([FromBody] PropertyResource propertyResource)
         {
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
             var property = Mapper.Map<PropertyResource, Property>(propertyResource);
+            context.Properties.Add(property);
+            await context.SaveChangesAsync();
             return Ok(property);
         }
 
-        [HttpGet]
-        public async Task<IEnumerable<PropertyResource>> GetProperties()
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProperty(int id)
+        {
+            var property = await context.Properties.FindAsync(id);
+
+            if (property == null)
+            {
+                return NotFound();
+            } 
+
+            var propertyResource = Mapper.Map<Property, PropertyResource>(property);
+            return Ok(propertyResource);
+        }
+
+        [HttpGet("list")]
+        public async Task<IEnumerable<PropertyResource>> GetListOfProperties(int id)
         {
             var listOfProperties = await context.Properties.Include(m => m.Wholesaler).ToListAsync();
 
             return Mapper.Map<List<Property>, List<PropertyResource>>(listOfProperties);
-        }          
+        }        
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProperty(int id, [FromBody] PropertyResource propertyResource)
+        {
+            var property = await context.Properties.FindAsync(id);
+            if (!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+            if (property == null) 
+            {
+                return NotFound();
+            }            
+            
+            Mapper.Map<PropertyResource, Property>(propertyResource, property);
+            
+            await context.SaveChangesAsync();
+            return Ok(property);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProperty(int id)
+        {
+            var property = await context.Properties.FindAsync(id);
+            
+            if (property == null) 
+            {
+                return NotFound();
+            }
+            context.Remove(property);
+            await context.SaveChangesAsync();
+
+            return Ok(id);
+        }
 
     }
 }

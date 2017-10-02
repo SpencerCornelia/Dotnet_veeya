bc46fd66e8533485e7aa1d44c000562fcbc2a5e4
Spencer Cornelia <cornelia.spencer@yahoo.com>
2017-10-01 18:39:22 -0700

    added new Property properties

--- Controllers/Resources/PropertyResource.cs
+++ Controllers/Resources/PropertyResource.cs
@@ -13,6 +13,29 @@ namespace Veeya.Controllers.Resources
         public string AddressName { get; set; }
         [Required]
         public string WholesalerId { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public int ZipCode { get; set; }
+        [Required]
+        public int PurchasePrice { get; set; }
+        [Required]
+        public int Bedrooms { get; set; }
+        [Required]
+        public int Bathrooms { get; set; }
+        [Required]
+        public int RehabCostMin { get; set; }
+        [Required]
+        public int RehabCostMax { get; set; }
+        [Required]
+        public int AfterRepairValue { get; set; }
+        public int AverageRent { get; set; }
+        public int SquareFootage { get; set; }
+        [Required]
+        public string PropertyType { get; set; }
+        public int YearBuilt { get; set; }      
       
     }
 }

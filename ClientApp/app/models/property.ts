57917d4b121ac78917503f3d73fe7fcc6a2ee194
Spencer Cornelia <cornelia.spencer@yahoo.com>
2017-10-05 05:59:27 -0700

    most recent configuring

---
+++ ClientApp/app/models/property.ts
@@ -0,0 +1,18 @@
+export interface Property {
+    PropertyId: number,
+    AddressName: string,
+    WholesalerId: number,
+    City: string,
+    State: string,
+    ZipCode: number,
+    PurchasePrice: number,
+    Bedrooms: number,
+    Bathrooms: number,
+    RehabCostMin: number,
+    RehabCostMax: number,
+    AfterRepairValue: number,
+    AverageRent: number,
+    SquareFootage: number,
+    PropertyType: string,
+    YearBuilt: number
+}
